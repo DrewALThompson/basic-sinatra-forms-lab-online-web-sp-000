8b8f63fed4835782aed04e14a1b9d1599a31843d
Drew Thompson <drewallynthompson@gmail.com>
2020-11-01 07:27:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
   
   get '/team/:name' do
-    @team = Hash.new(name: params[:name], coach: params[:coach],
+    @team = Hash.new(name: params[:name], coach: params[:coach], pg: params[:pg], sg: params[:sg], pf: params[pf], center: params[:center])
 
 
 end
